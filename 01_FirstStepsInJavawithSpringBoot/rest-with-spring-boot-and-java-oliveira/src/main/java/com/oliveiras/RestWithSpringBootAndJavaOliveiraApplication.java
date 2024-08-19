8197c2cc4ceb94b0c390e3c80679f0fc81e124b3
Elson Oliveira <oelson.oliveiras@gmail.com>
2024-08-19 19:59:24 -0400

    First Steps in Java with Spring Boot

---
+++ 01_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-oliveira/src/main/java/com/oliveiras/RestWithSpringBootAndJavaOliveiraApplication.java
@@ -0,0 +1,13 @@
+package com.oliveiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaOliveiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaOliveiraApplication.class, args);
+	}
+
+}
